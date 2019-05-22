3fad22884195fd68cab41ba5457ee7fcbfa7b0a8
Wayne Hsu <fdj184@gmail.com>
2019-05-23 00:38:46 +0800

    Exercise 2 in section 3

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -44,7 +44,7 @@ namespace Vidly.Controllers
             //var customer = GetCustomers().SingleOrDefault(c => c.Id == Id);
 
             // 改成從 ApplicationDbContext 取得客戶資料
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == Id);
 
             if (customer == null)
                 return HttpNotFound();
