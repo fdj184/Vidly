1402c702d42918ecd0e90a800ba641b8b175a9e6
Wayne Hsu <fdj184@gmail.com>
2019-05-23 02:55:30 +0800

    Exercise 3 in section 3

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,12 +11,37 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        // 建構式，初始化 ApplicationDbContext 物件
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // 覆寫 Dispose()，讓 ApplicationDbContext 物件可以釋放資源
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            // 原本 hard code 客戶資料
+            //var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre);
 
             return View(movies);
+        }
+
+        // GET: Movies/Details
+        [Route("movies/details/{Id:regex(\\d+)}")]
+        public ActionResult Details(int Id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == Id);
+
+            return View(movie);
         }
 
         // GET: Movies/Random
@@ -39,13 +65,14 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>()
-            {
-                new Movie { Name = "Shrek" },
-                new Movie { Name = "Wall-e" }
-            };
-        }
+        // 原本 hard code 客戶資料
+        //private IEnumerable<Movie> GetMovies()
+        //{
+        //    return new List<Movie>()
+        //    {
+        //        new Movie { Name = "Shrek" },
+        //        new Movie { Name = "Wall-e" }
+        //    };
+        //}
     }
 }
