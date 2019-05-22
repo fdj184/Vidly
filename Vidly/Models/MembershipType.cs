38897aa15202625becb5bf04e8bc86901b4b1fd0
Wayne Hsu <fdj184@gmail.com>
2019-05-22 16:30:20 +0800

    Add properties to Customer class and add MembershipType class to the project

---
+++ Vidly/Models/MembershipType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class MembershipType
+    {
+        // Id is PK column
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DuraionInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
