9ff32e212ccaf05f043938f4d48afa17160bc624
Wayne Hsu <fdj184@gmail.com>
2019-05-24 18:29:40 +0800

    Saving New Data

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -73,5 +73,14 @@ namespace Vidly.Controllers
 
             return View(viewModel);
         }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
     }
 }
