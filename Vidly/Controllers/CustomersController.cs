a68b3b9331117f3dc5a58c2a2887336585c544e9
Wayne Hsu <fdj184@gmail.com>
2019-05-24 17:09:27 +0800

    Add Drop-down Lists

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -64,8 +65,13 @@ namespace Vidly.Controllers
 
         public ActionResult New()
         {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel()
+            {
+                MembershipTypes = membershipTypes
+            };
 
-            return View();
+            return View(viewModel);
         }
     }
 }
--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -23,6 +23,7 @@ namespace Vidly.Models
         // 自定義類別
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
