efd2b3dca71e14bdca931ac23ec5e973cd6421fa
Wayne Hsu <fdj184@gmail.com>
2019-05-29 19:19:40 +0800

    Building an API

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // GET /api/customers/{id}
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        // POST /api/customers
+        [System.Web.Http.HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // PUT /api/customers/{id}
+        [System.Web.Http.HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDB == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            customerInDB.Name = customer.Name;
+            customerInDB.Birthday = customer.Birthday;
+            customerInDB.MembershipTypeId = customer.MembershipTypeId;
+            customerInDB.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/customers/{id}
+        [System.Web.Http.HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDB == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customerInDB);
+
+            _context.SaveChanges();
+        }
+    }
+}
