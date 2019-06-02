c228e66ed9d64c6495e0019c56909ed4fac35215
Wayne Hsu <fdj184@gmail.com>
2019-06-02 23:11:04 +0800

    Adding Profile Data

--- Vidly/Models/AccountViewModels.cs
+++ Vidly/Models/AccountViewModels.cs
@@ -79,6 +79,11 @@ namespace Vidly.Models
         [Display(Name = "確認密碼")]
         [Compare("Password", ErrorMessage = "密碼和確認密碼不相符。")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "駕照號碼")]
+        public string DrivingLicense { get; set; }
     }
 
     public class ResetPasswordViewModel
