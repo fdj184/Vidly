a45d219ed170a260821e24d87e787b9b73d8c476
Wayne Hsu <fdj184@gmail.com>
2019-06-03 15:41:40 +0800

    Exercise in section 8: Restrict movie APIs operations to admin

--- Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -53,6 +53,7 @@ namespace Vidly.Controllers.Api
 
         // POST /api/movies
         [HttpPost]
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public IHttpActionResult CreateMovie(MovieDto movieDto)
         {
             if (!ModelState.IsValid)
@@ -71,6 +72,7 @@ namespace Vidly.Controllers.Api
 
         // PUT /api/movies/{id}
         [HttpPut]
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public IHttpActionResult UpdateMovie(int id, MovieDto movieDto)
         {
             if (!ModelState.IsValid)
@@ -89,6 +91,7 @@ namespace Vidly.Controllers.Api
 
         // DELETE /api/movies/{id}
         [HttpDelete]
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public IHttpActionResult DeleteMovie(int id)
         {
             var movieInDB = _context.Movies.SingleOrDefault(m => m.Id == id);
