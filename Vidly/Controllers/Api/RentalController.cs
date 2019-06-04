4b95cb732077fe7b4736980652917ef30e1d755d
Wayne Hsu <fdj184@gmail.com>
2019-06-04 18:51:42 +0800

    Building a new Rental Feature - Create Models, DTOs and empty Action with API

---
+++ Vidly/Controllers/Api/RentalController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Security;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class RentalController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public RentalController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // POST /api/rental
+        [HttpPost]
+        public IHttpActionResult New(RentalDto rentalDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
